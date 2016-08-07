91b9b441094a87c52de35b76778b1303dc026b20
npentella <npentella@gmail.com>
2016-08-06 19:46:59 -0500

    Create interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,8 +5,6 @@ def encrypt(password)
 	
 	#While index is less than word length, 
 	#advance corresponding letter one character, except `space`
-	
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 	index = 0
 
@@ -18,31 +16,82 @@ def encrypt(password)
 		end
 		index += 1
 	end
-	puts password
+	return password
 end
 
-encrypt("not hungry")
+#puts encrypt("not hungry")
 
 #DECRYPT METHOD
 
-def decrypt(encrypted_password)
+def decrypt(password)
 
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 	index = 0
 
-	while index < encrypted_password.length
-		if encrypted_password[index] != " "
-			encrypted_password[index] = alphabet[alphabet.index(encrypted_password[index]) - 1]
+	while index < password.length
+		if password[index] != " "
+			password[index] = alphabet[alphabet.index(password[index]) - 1]
 		end
 		index += 1
 	end
-	puts encrypted_password
+	return password
 end
 
-decrypt("opu ivohsz")
+# puts decrypt("opu ivohsz")
 
-encrypt("abc")
-encrypt("zed")
-decrypt("bcd")
-decrypt("afe")
+#puts encrypt("abc")
+#puts encrypt("zed")
+#puts decrypt("bcd")
+#puts decrypt("afe")
+
+#puts decrypt(encrypt("swordfish"))
+
+#This initially gave us an error.  After some research, we learned
+# that using `puts` instead of `return` was not returning an actual
+# value for the decrypt method to use.
+
+#INTERFACE
+
+#Asks user wether they want to encrypt or decrypt password
+
+#Asks for password
+
+#If user selected encrypt, call encrypt method; and vice versa
+type_input = nil
+
+until (type_input == "encrypt") || (type_input == "decrypt")
+	puts "Would you like to encrypt or decrypt?"
+	type_input = gets.chomp
+	if (type_input != "encrypt") && (type_input != "decrypt")
+		puts "Please be a better secret agent and type encrypt or decrypt!"
+	end
+end
+
+puts "Enter password:"
+
+user_password = gets.chomp
+
+if type_input == "encrypt"
+	puts encrypt(user_password)
+	elsif type_input == "decrypt"
+		puts decrypt(user_password)
+end
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
