41e2d6edb1c2fc09bf929bd8a61c33eaae921666
npentella <npentella@gmail.com>
2016-08-10 17:34:47 -0500

    Collect number of children, preferred decor, favorite color

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,7 +6,7 @@ client_prefs = {}
 
 #Ask for client name, store in hash
 
-puts "what is the client's full name?"
+puts "What is the client's full name?"
 client_prefs[:name] = gets.chomp
 
 #Client age
@@ -17,9 +17,19 @@ client_prefs[:age] = client_prefs[:age].to_i
 
 #Number of children
 
+puts "How many children does the client have?"
+client_prefs[:children] = gets.chomp
+client_prefs[:children] = client_prefs[:children].to_i
+
 #Decor theme
 
+puts "What is the client's preferred decor theme?"
+client_prefs[:theme] = gets.chomp
+
 #favorite color
+
+puts "What is the client's favorite color?"
+client_prefs[:color] = gets.chomp
 
 #Print hash
 p client_prefs
