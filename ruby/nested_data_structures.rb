86c2093f8492aed91c9a051fd5b31552f5763a5c
npentella <npentella@gmail.com>
2016-08-12 18:59:52 -0500

    Create data structure for Rio Olympics

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,108 @@
+rio_2016 = {
+	sports: {
+		swimming: {
+			venues: ["Rio Aquatic Center", "Atlantic Ocean"]
+			medalists: {
+				mens_freestyle: {
+					gold: {
+						athlete: "Michael Phelps"
+						country: "USA"
+					}
+					silver: {
+						athlete: "Ryan Lockte"
+						country: "USA"
+					}
+					bronze: {
+						athlete: "Kyle Chalmers"
+						country: "Australia"
+					}
+				}
+				womens_freestyle: {
+					gold: {
+						athlete: "Katie Ledecky"
+						country: "USA"
+					}
+					silver: {
+						athlete: "Lily King"
+						country: "USA"
+					}
+					bronze: {
+						athlete: "Yulia Efimova"
+						country: "Russia"
+					}
+				}
+			}
+		}
+		basketball: {
+			venues: ["Rio Olympic Arena", "Playground"]
+			medalists: {
+				gold: {
+					country: "USA"
+					wins: 10
+					losses: 0
+				}
+				silver: {
+					country: "Australia"
+					wins: 8
+					losses: 2
+				}
+				bronze: {
+					country: "China"
+					wins: 6
+					losses: 3
+				}
+			}
+			all_olympic_team: ["Demarcus Cousins", "Kyrie Irving", "Carmelo Anthony", "Matthew Delavedova", "Kevin Durant"]
+		}
+		gymnastics: {
+			venues: ["Olympic Gymnastics Center", "Monkey Bars"]
+			events: {
+				mens_team: {
+					medalists: {
+						gold: "USA"
+						silver: "China"
+						bronze: "Russia"
+					}
+					overall_winner: "Johnny Gymnastics"
+				}
+				womens_team: {
+					medalists: {
+						gold: "USA"
+						silver: "Russia"
+						bronze: "China"
+					}
+					overall_winner: "Simone Biles"
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
