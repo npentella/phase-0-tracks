2d580815ec3bb18e52396f37130c740d30f24eb4
npentella <npentella@gmail.com>
2016-08-12 20:57:36 -0500

    debug data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,91 +1,119 @@
-rio_2016 = {
+rio_olympics = {
 	sports: {
 		swimming: {
-			venues: ["Rio Aquatic Center", "Atlantic Ocean"]
-			medalists: {
+			venues: [ "Rio Aquatic Center", "Atlantic Ocean" ],
+			events: {
 				mens_freestyle: {
-					gold: {
-						athlete: "Michael Phelps"
-						country: "USA"
-					}
-					silver: {
-						athlete: "Ryan Lockte"
-						country: "USA"
-					}
-					bronze: {
-						athlete: "Kyle Chalmers"
-						country: "Australia"
-					}
-				}
+					medalists: {
+						gold: {
+							athlete: "Michael Phelps",
+							country: "USA"
+							},
+						silver: {
+							athlete: "Ryan Lockte",
+							country: "USA"
+							},
+						bronze: {
+							athlete: "Kyle Chalmers",
+							country: "Australia"
+						}
+					},
+					world_record: true
+				},
 				womens_freestyle: {
-					gold: {
-						athlete: "Katie Ledecky"
-						country: "USA"
-					}
-					silver: {
-						athlete: "Lily King"
-						country: "USA"
-					}
-					bronze: {
-						athlete: "Yulia Efimova"
-						country: "Russia"
-					}
+					medalists: {
+						gold: {
+							athlete: "Katie Ledecky",
+							country: "USA"
+							},
+						silver: {
+							athlete: "Lily King",
+							country: "USA"
+							},
+						bronze: {
+							athlete: "Yulia Efimova",
+							country: "Russia"
+						}
+					},
+					world_record: true
 				}
 			}
-		}
+		},
 		basketball: {
-			venues: ["Rio Olympic Arena", "Playground"]
+			venues: ["Rio Olympic Arena", "Playground"],
 			medalists: {
 				gold: {
-					country: "USA"
-					wins: 10
+					country: "USA",
+					wins: 10,
 					losses: 0
-				}
+				},
 				silver: {
-					country: "Australia"
-					wins: 8
+					country: "Australia",
+					wins: 8,
 					losses: 2
-				}
+				},
 				bronze: {
-					country: "China"
-					wins: 6
+					country: "Spain",
+					wins: 6,
 					losses: 3
 				}
-			}
+			},
 			all_olympic_team: ["Demarcus Cousins", "Kyrie Irving", "Carmelo Anthony", "Matthew Delavedova", "Kevin Durant"]
-		}
+		},
 		gymnastics: {
-			venues: ["Olympic Gymnastics Center", "Monkey Bars"]
+			venues: ["Olympic Gymnastics Center", "Monkey Bars"],
 			events: {
-				mens_team: {
-					medalists: {
-						gold: "USA"
-						silver: "China"
-						bronze: "Russia"
-					}
-					overall_winner: "Johnny Gymnastics"
-				}
 				womens_team: {
 					medalists: {
-						gold: "USA"
-						silver: "Russia"
-						bronze: "China"
-					}
+						gold: "USA",
+						silver: "China",
+						bronze: "Russia"
+					},
 					overall_winner: "Simone Biles"
+				},
+				mens_team: {
+					medalists: {
+						gold: "USA",
+						silver: "Russia",
+						bronze: "China"
+					},
+					overall_winner: "Johnny Gymnastics"
 				}
 			}
+		}
+	},
+	countries: {
+		usa: {
+			pretty_name: "USA",
+			medal_count: {
+				gold: 16,
+				silver: 12,
+				bronze: 16
+			},
+			athlete_count: 555
+		},
+		china: {
+			pretty_name: "China",
+			medal_count: {
+				gold: 13,
+				silver: 10,
+				bronze: 14
+			},
+			athlete_count: 401
+		},
+		great_britain: {
+			pretty_name: "Great Britain",
+			medal_count: {
+				gold: 7,
+				silver: 8,
+				bronze: 6
+			},
+			athlete_count: 372
 		}
 	}
 }
 
-
-
-
-
-
-
-
-
+p rio_olympics[:countries][:usa][:medal_count].values.each.reduce(:+)
 
 
 
