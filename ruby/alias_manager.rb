52420854304583f24d4da708b235cd8f15f82e8f
npentella <npentella@gmail.com>
2016-08-13 17:37:24 -0500

    Add user interface with loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,57 +1,69 @@
-# Ask for spy's real name.
+#Loop until user enters "quit"
+
+name_input = nil
 
 puts "Hello, Agent." 
-puts "What is your real name, first and last?  We will generate a coded alias."
-name_input = gets.chomp
 
-# CREATE FAKE NAME
+until name_input == "quit"
 
-#Create arrays of uppercase and lowercase consonants and vowels
+	# Ask for spy's real name.
 
-big_vowels = "AEIOU".chars
-little_vowels = "aeiou".chars
-big_consonants = "BCDFGHJKLMNPQRSTVWXYZ".chars
-little_consonants = "bcdfghjklmnpqrstvwxyz".chars
+	puts "What is your real name, first and last?  We will generate a coded alias for each name you enter until you enter quit."
+	name_input = gets.chomp
 
-# Swap first and last name
+	if name_input != "quit"
+	
 
-code_name = name_input.split(" ").reverse.join(" ")
+		# CREATE FAKE NAME
 
-# p code_name
+		#Create arrays of uppercase and lowercase consonants and vowels
 
-# Change all vowels to next vowel (aeiou)
-# Change all consonants to the next consonant in the alphabet
+		big_vowels = "AEIOU".chars
+		little_vowels = "aeiou".chars
+		big_consonants = "BCDFGHJKLMNPQRSTVWXYZ".chars
+		little_consonants = "bcdfghjklmnpqrstvwxyz".chars
 
-code_chars = code_name.chars
+		# Swap first and last name
 
-# p code_chars
+		code_name = name_input.split(" ").reverse.join(" ")
 
-code_chars.map! do |c|
-	if big_vowels.include?(c)
-		if big_vowels.index(c) < 4
-			c = big_vowels[big_vowels.index(c) + 1]
-		else c = big_vowels[0]
+		# p code_name
+
+		# Change all vowels to next vowel (aeiou)
+		# Change all consonants to the next consonant in the alphabet
+
+		code_chars = code_name.chars
+
+		# p code_chars
+
+		code_chars.map! do |c|
+			if big_vowels.include?(c)
+				if big_vowels.index(c) < 4
+					c = big_vowels[big_vowels.index(c) + 1]
+				else c = big_vowels[0]
+				end
+			elsif little_vowels.include?(c)
+				if little_vowels.index(c) < 4
+					c = little_vowels[little_vowels.index(c) + 1]
+				else c = little_vowels[0]
+				end
+			elsif big_consonants.include?(c)
+				if big_consonants.index(c) < 20
+					c = big_consonants[big_consonants.index(c) + 1]
+				else c = big_consonants[0]
+				end
+			elsif little_consonants.include?(c)
+				if little_consonants.index(c) < 20
+					c = little_consonants[little_consonants.index(c) + 1]
+				else c = little_consonants[0]
+				end
+			else c = c
+			end
 		end
-	elsif little_vowels.include?(c)
-		if little_vowels.index(c) < 4
-			c = little_vowels[little_vowels.index(c) + 1]
-		else c = little_vowels[0]
-		end
-	elsif big_consonants.include?(c)
-		if big_consonants.index(c) < 20
-			c = big_consonants[big_consonants.index(c) + 1]
-		else c = big_consonants[0]
-		end
-	elsif little_consonants.include?(c)
-		if little_consonants.index(c) < 20
-			c = little_consonants[little_consonants.index(c) + 1]
-		else c = little_consonants[0]
-		end
-	else c = c
 	end
-end
 
-puts code_chars.join
+	puts "Here is your final code name: #{code_chars.join}"
+end
 
 
 
