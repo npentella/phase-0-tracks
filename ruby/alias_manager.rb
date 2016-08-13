037f80e2798374830b085ecd8b1894148ec58ff0
npentella <npentella@gmail.com>
2016-08-13 17:28:52 -0500

    Complete encoding logic for release 0

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,15 +10,72 @@ name_input = gets.chomp
 
 big_vowels = "AEIOU".chars
 little_vowels = "aeiou".chars
-big_consonants = "BCDFGHJKLMNPQRSTVXYZ"
-little_consonants = "bcdfghjklmnpqrstvxyz"
+big_consonants = "BCDFGHJKLMNPQRSTVWXYZ".chars
+little_consonants = "bcdfghjklmnpqrstvwxyz".chars
 
 # Swap first and last name
 
 code_name = name_input.split(" ").reverse.join(" ")
 
-p code_name
+# p code_name
 
 # Change all vowels to next vowel (aeiou)
 # Change all consonants to the next consonant in the alphabet
+
+code_chars = code_name.chars
+
+# p code_chars
+
+code_chars.map! do |c|
+	if big_vowels.include?(c)
+		if big_vowels.index(c) < 4
+			c = big_vowels[big_vowels.index(c) + 1]
+		else c = big_vowels[0]
+		end
+	elsif little_vowels.include?(c)
+		if little_vowels.index(c) < 4
+			c = little_vowels[little_vowels.index(c) + 1]
+		else c = little_vowels[0]
+		end
+	elsif big_consonants.include?(c)
+		if big_consonants.index(c) < 20
+			c = big_consonants[big_consonants.index(c) + 1]
+		else c = big_consonants[0]
+		end
+	elsif little_consonants.include?(c)
+		if little_consonants.index(c) < 20
+			c = little_consonants[little_consonants.index(c) + 1]
+		else c = little_consonants[0]
+		end
+	else c = c
+	end
+end
+
+puts code_chars.join
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
