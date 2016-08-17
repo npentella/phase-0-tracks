c7ea1f47927d4bce5c1917454a406f76bb2314ce
npentella <npentella@gmail.com>
2016-08-16 19:15:25 -0500

    Add pseudocode on puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# BREED(S)------------
+# Terrier
+# 
+# CHARACTERISTICS
+# Name: Varies
+# Age: Varies
+# Shelter: Varies
+# 
+# APPEARANCE
+# Weight: Varies
+# Ear Count: 2
+# Eye Count: 2
+# Fur Color: Varies
+# 
+# BEHAVIOR
+# Yappy
+# Playful
+# Friendly
