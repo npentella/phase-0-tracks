71be19b381550c814d526d5c4e76fc8dbc40592f
npentella <npentella@gmail.com>
2016-08-17 13:37:20 -0500

    Add initialize method to puppy class and start building pokemon class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,31 +1,38 @@
 class Puppy
+	def fetch(toy)
+		puts "I brought back the #{toy}!"
+	end
 
- def fetch(toy)
-   puts "I brought back the #{toy}!"
-   toy
- end
+	def speak(num)
+		puts 'woof' * num
+	end
 
- def speak(num)
-     puts 'woof' * num 
- end
+	def roll_over
+		puts "*roll over*"
+	end
 
- def rollover
-     puts "*roll over*"
- end
+	def dog_years(age)
+		puts age * 7
+	end
 
- def dog_years(age)
-     puts age * 7
- end
+	def play_dead(length)
+		puts "plays dead for #{length} seconds."
+	end
 
- def play_dead(length)
-     puts "plays dead for #{length} per seconds."
- end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+end
 
+class Pokemon
+	def initialize
+		"Adding pokemon to Pokedex..."
+	end
 end
 
 spot = Puppy.new 
 spot.fetch('bone')
 spot.speak(3)
-spot.rollover
+spot.roll_over
 spot.dog_years(14)
 spot.play_dead(60)
