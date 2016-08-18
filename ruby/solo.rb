9dfa7d83c9bc3b67cfdff846f46c620d47134806
npentella <npentella@gmail.com>
2016-08-18 14:52:55 -0500

    Pseudocode ky bball player class

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+# -- KENTUCKY BASKETBALL PLAYERS --
+
+# CHARACTERISTICS
+# Name - varies
+# Height - varies
+# Position - varies
+# Years in School
+# Accolades
+# Points per game
+# Won championship?
+# Coach - defaults to john calipari
+
+# BEHAVIOR
+# Hit the three
+# Slam dunk
+# Beat Louisville
+# Win title
+# Get Drafted
+# Commit to Kentucky
