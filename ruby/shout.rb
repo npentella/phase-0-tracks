5d31230c68f7ec5f837535fe1e4973e0a3b45623
npentella <npentella@gmail.com>
2016-08-21 18:38:43 -0400

    Convert shout module to mix-in

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,43 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+# 
+#   def self.yell_happily(words)
+#   	"OMG!" + words + "!!! :)"
+#   end
+# end
 
-  def self.yell_happily(words)
-  	"OMG!" + words + "!!! :)"
-  end
+module Shout
+	def yell_angrily(words)
+  		words + "!!!" + " :("
+	end
+	def yell_happily(words)
+		"OMG!" + words + "!!! :)"
+	end	
+end
+
+class Coder
+	include Shout
+end
+
+class Coach
+	include Shout
 end
 
 # DRIVER CODE #
 
-puts Shout.yell_angrily("Eeeeek")
-puts Shout.yell_happily("Oooooh")
+coder = Coder.new
+puts coder.yell_angrily("Darn")
+
+coach = Coach.new
+puts coach.yell_happily("We Win")
+
+
+
+
+
+
+
+
+
