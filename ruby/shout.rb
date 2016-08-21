fd9a037cbd089f5fb51adc04d7d377b7cd00ab14
npentella <npentella@gmail.com>
2016-08-21 18:11:21 -0400

    Add two methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	"OMG!" + words + "!!! :)"
+  end
 end
+
+# DRIVER CODE #
+
+puts Shout.yell_angrily("Eeeeek")
+puts Shout.yell_happily("Oooooh")
