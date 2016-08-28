5fef751dabafd3b69421c244f9a7a1ae10d0836b
npentella <npentella@gmail.com>
2016-08-28 18:45:11 -0500

    Add reverse function

---
+++ js/explore.js
@@ -0,0 +1,17 @@
+// REVERSE FUNCTION
+// INPUT: a string, ie "hello"
+// OUTPUT: a string with reversed characters, ie "olleh"
+// Start at index -1 of inputed string
+// Add each character to new string and move to next negative index
+
+function reverse(string) {
+	var reversedString = "";
+	for (var i = string.length - 1; i >= 0; i--) {
+		reversedString += string[i];
+	}
+	return reversedString;
+}
+
+// DRIVER CODE
+
+console.log(reverse("Hello"));
