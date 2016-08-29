02c280d070f47852d9325f4270acf6ba3826bc66
npentella <npentella@gmail.com>
2016-08-28 20:22:40 -0500

    Add name and color arrays

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors = ["maize", "blue", "white", "gray"];
+
+var names = ["Gomer", "Bert", "Jesse", "Kaileigh"];
+
+colors.push("green")
+
+names.push("Sharpy")
+
+console.log(colors)
+console.log(names)
