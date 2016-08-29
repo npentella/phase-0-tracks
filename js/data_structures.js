dd6237bafafc23a446ada8dbf07b8d6f3e3da858
npentella <npentella@gmail.com>
2016-08-28 21:08:46 -0500

    Build loop to assign colors to horses

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,5 +6,18 @@ colors.push("green")
 
 names.push("Sharpy")
 
+horseData = {}
+
+// Color Assigner
+// For each name
+// Assign color with matching index #
+// Add to object
+
+for (var i = 0; i < names.length; i++) {
+	horseData[names[i]] = colors[i]
+}
+
+// Driver Code
 console.log(colors)
 console.log(names)
+console.log(horseData)
