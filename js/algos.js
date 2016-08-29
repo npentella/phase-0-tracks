02bc124dbf30245c083a681788853c05725fe117
npentella <npentella@gmail.com>
2016-08-28 23:21:13 -0500

    Add function to check objects for matching key value pairs

--- js/algos.js
+++ js/algos.js
@@ -19,6 +19,25 @@ function findLongest(phrases) {
 	return longest
 };
 
+// CHECK IF OBJECTS SHARE KEY VALUE PAIR
+// Input: Two objects
+// Output: True if there is a shared key/value pair
+// Set 'pairMatch' var to false
+// For each key in object 1
+	// Check each key in object 2 for equivalence
+	// If keys and values are equal,
+			// set 'pairMatch' var to true
+
+function pairMatch(object1, object2) {
+	var pairMatch = false;
+	for (var i = 0; i < Object.keys(object1).length - 1; i++) {
+		if ((Object.keys(object1)[i] == Object.keys(object2)[i]) && (object1[Object.keys(object1)[i]] == object2[Object.keys(object2)[i]])) {
+			pairMatch = true;
+		}
+	}
+	return pairMatch
+};
+
 // DRIVER CODE
 
 var testArray = ["long", "longer", "most long"];
@@ -30,3 +49,31 @@ console.log(findLongest(testArray));
 console.log(findLongest(testArray2));
 
 console.log(findLongest(testArray3));
+
+var cat1 = {
+	name: "gomer",
+	color: "white",
+	age: 9
+};
+
+var cat2 = {
+	name: "rue",
+	color: "black",
+	age: 2
+};
+
+var cat3 = {
+	name: "sophie",
+	color: "black",
+	age: 8
+};
+
+console.log(pairMatch(cat1, cat2));
+console.log(pairMatch(cat2, cat3));
+
+
+
+
+
+
+
