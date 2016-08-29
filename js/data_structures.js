37cd9b8c7c43fa3fc3969759a550a4dd0688155b
npentella <npentella@gmail.com>
2016-08-28 21:19:33 -0500

    Create Dog object

--- js/data_structures.js
+++ js/data_structures.js
@@ -21,3 +21,21 @@ for (var i = 0; i < names.length; i++) {
 console.log(colors)
 console.log(names)
 console.log(horseData)
+
+function Car(make, model, year, color, miles) {
+
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this.miles = miles;
+
+	function getPainted(newColor) {
+		this.color = newColor;
+	};
+
+}
+
+var firstCar = new Car("Ford", "Mustang", 1967, "purple", 2);
+
+console.log(firstCar);
