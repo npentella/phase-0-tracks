6497d36d0a6dc7ad49c538befa24012489b9a165
npentella <npentella@gmail.com>
2016-09-08 17:13:00 -0500

    Use javascript to style shet and add event listener

--- web_dev/client_side_js/scripts.js
+++ web_dev/client_side_js/scripts.js
@@ -1,1 +1,13 @@
 console.log("Scripts running!");
+
+var paragraphs = document.getElementsByClassName('content');
+
+function addBlueBorder(event) {
+	for (var i = 0; i < paragraphs.length; i++) {
+	paragraphs[i].style.border = "10px solid blue";
+	};
+}
+
+var header = document.getElementById("firstHeader");
+
+header.addEventListener("click", addBlueBorder);
